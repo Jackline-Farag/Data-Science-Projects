43eddaef109e6870c40a98b62af73e5b26bfb5c6
Jackline Farag <154635150+Jackline-Farag@users.noreply.github.com>
2024-03-03 21:22:19 +0000

    Comments added to while.py

--- while.py
+++ while.py
@@ -2,14 +2,16 @@ counter=0
 Total=0
 print('''You will be asked to enter a number and an average of the numbers entered is calculated, 
 entering -1 will result in quiting the program\n''')
+
+# The while loop will continue until the number eentered is -1
 while True:
-   
     number= float(input("Enter a number:  "))
     if number!=-1:
         Total=Total+number
         counter+=1
         
     else:
+ # This will display the average of the numbers entered by the user 
         Average=Total/counter
         print (f"The average of the numbers entered is {Average}")
         break
